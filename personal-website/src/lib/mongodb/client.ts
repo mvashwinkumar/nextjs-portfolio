a1033fa12c725d26c5c3410dd56e28b0c05e29a6
Ashwin Kumar <ashwin.kumar@aiquris.com>
2024-11-24 01:21:38 +0800

    update mongo stub error

--- personal-website/src/lib/mongodb/client.ts
+++ personal-website/src/lib/mongodb/client.ts
@@ -1,11 +1,7 @@
 ﻿import { MongoClient, ConnectOptions } from 'mongodb';
 
 const uri = process.env.MONGODB_URI as string;
-const options: ConnectOptions = {
-  maxPoolSize: 10,
-  minPoolSize: 5,
-  retryWrites: true,
-};
+const options: ConnectOptions = {};
 
 // TODO: Implement connection pooling and retry logic
 let client: MongoClient;
